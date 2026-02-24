05abc79b7c736c6f54f84a292c84bdb0efd398e1
Tommy Shelburne <tshelburne02@gmail.com>
2026-02-24 11:31:02 -0700

    fix: add CORS policy and pin Vite dev server to port 5173

--- server/Program.cs
+++ server/Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IWorkItemService, WorkItemService>();
+builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(policy =>
+        policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod()));
 
 var app = builder.Build();
 
@@ -14,6 +17,7 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+app.UseCors();
 app.MapControllers();
 
 app.Run();
